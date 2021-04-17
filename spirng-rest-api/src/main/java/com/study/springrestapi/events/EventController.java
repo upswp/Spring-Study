29d3149d24cc8a033c7c6646a6b77d94f9dbf83d
Park Sangwoo <tkddnsos@gmail.com>
2021-04-17 10:03:18 +0900

    [feat] : POST /api/events returns 201 & create repository & refactor Controller

--- spirng-rest-api/src/main/java/com/study/springrestapi/events/EventController.java
+++ spirng-rest-api/src/main/java/com/study/springrestapi/events/EventController.java
@@ -1,28 +1,40 @@
 package com.study.springrestapi.events;
 
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.swing.border.EmptyBorder;
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Controller
+@RequestMapping(value = "/api/events",produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
 
-    @PostMapping("/api/events/")
+    private final EventRepository eventRepository;
+
+    public EventController(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @PostMapping
     /**
      * ResponseEntity를 사용하는 이유 : 응답 코드, 헤더, 본문 모두 다루기 편한 API
      */
-    public ResponseEntity createEvent(){
+    public ResponseEntity createEvent(@RequestBody Event event){
+        Event newEvent = this.eventRepository.save(event);
         /**
          * create를 만들때는 항상 URI가 필요하다.
          * Location URI 만들기 : HATEOS가 제공하는 linkTo(), methodOn() 사용
          * 객체를 JSON으로 변환 : Object Mapper 사용
          */
-        URI createUri = linkTo(methodOn(EventController.class).createEvent()).slash("{id}").toUri();
-        return ResponseEntity.created(createUri).build();
+        URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
+        return ResponseEntity.created(createUri).body(event);
     }
 }
---
+++ spirng-rest-api/src/main/java/com/study/springrestapi/events/EventRepository.java
@@ -0,0 +1,6 @@
+package com.study.springrestapi.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+}
