bd89c4c95589228fab13efcd057af3358eb0427c
Park Sangwoo <tkddnsos@gmail.com>
2021-04-19 00:32:07 +0900

    [chore] : Setting Main DB & Test DB

--- spirng-rest-api/src/test/java/com/study/springrestapi/events/EventControllerTests.java
+++ spirng-rest-api/src/test/java/com/study/springrestapi/events/EventControllerTests.java
@@ -18,6 +18,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -52,6 +53,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 public class EventControllerTests {
 
     /**
